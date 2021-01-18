eaf44a0bcd61ef0795c004be7de9d37221114eaf
Jared Roberts <developerjared@gmail.com>
2021-01-18 13:09:13 +1300

    Person -> User

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 require "../bootstrap.php";
-use Src\Controller\PersonController;
+use Src\Controller\UsersController;
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
@@ -11,9 +11,9 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode( '/', $uri );
 
-// all of our endpoints start with /person for now
+// all of our endpoints start with /user for now
 // everything else results in a 404 Not Found this will change
-if ($uri[1] !== 'person') {
+if ($uri[1] !== 'user') {
     header("HTTP/1.1 404 Not Found");
     exit();
 }
@@ -26,6 +26,6 @@ if (isset($uri[2])) {
 
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 
-// pass the request method and user ID to the PersonController and process the HTTP request:
-$controller = new PersonController($dbConnection, $requestMethod, $userId);
+// pass the request method and user ID to the UserController and process the HTTP request:
+$controller = new UserController($dbConnection, $requestMethod, $userId);
 $controller->processRequest();
